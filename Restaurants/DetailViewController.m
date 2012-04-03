2a6e471d194dfb0e61dc07a9c4164e18910663d9
andymasteroffish <andymasteroffish@gmail.com>
2012-04-02 20:22:08 -0400

    Task 3 or assignment 4

--- Restaurants/DetailViewController.m
+++ Restaurants/DetailViewController.m
@@ -25,48 +25,6 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    restaurant = [[Restaurant alloc] init];
-    
-    restaurant.name = @"Pio Pio";
-    restaurant.address = @"746 First Avenue\nNew York, NY 10128";
-    restaurant.cuisineType = @"Peruvian";
-    restaurant.yearOpened = 1995;
-
-    Review* review1 = [[Review alloc] init];
-    review1.text = @"What fab-u-lass chicken! We could eat it all day if we didn't have to stop to drink sangria!";
-    review1.reviewer = @"The Addams";
-    review1.score = 5;
-    review1.numberOfHelpfulReviews = 19;
-    review1.numberOfUnhelpfulReviews = 8;
-    
-    Review* review2 = [[Review alloc] init];
-    review2.text = @"I DONE POSTED ON DA INTARWEBS!";
-    review2.reviewer = @"Anonymous";
-    review2.score = 1;
-    review2.numberOfHelpfulReviews = 0;
-    review2.numberOfUnhelpfulReviews = 45;
-    
-    Review* review3 = [[Review alloc] init];
-    review3.text = @"Some of the best chicken I've ever eaten. A helpful tip: get some green (Aji) sauce to go, they sell it by the pint!";
-    review3.reviewer = @"Jim Carr";
-    review3.score = 5;
-    review3.numberOfHelpfulReviews = 28;
-    review3.numberOfUnhelpfulReviews = 2;
-    
-    Review* review4 = [[Review alloc] init];
-    review4.text = @"While the food is amazing, they often simply don't pick up the phone when ordering out!";
-    review4.reviewer = @"Paul";
-    review4.score = 4;
-    review4.numberOfHelpfulReviews = 14;
-    review4.numberOfUnhelpfulReviews = 5;
-    
-    restaurant.reviews = [[NSMutableArray alloc] init];
-    [restaurant addReview:review1];
-    [restaurant addReview:review2];
-    [restaurant addReview:review3];
-    [restaurant addReview:review4];
-    //toss the reviews into the array
-    //restaurant.reviews = [[NSArray alloc] initWithObjects:review1, review2, review3,review4, nil];
     
     //set the text to be the most helpful review
     helpfulReviewLabel.text = [[restaurant mostHelpfulReview] text];
@@ -74,7 +32,7 @@
     
     //set the stars based on the review score
     NSLog(@"avg score: %f", [restaurant averageCustomerReview] );
-
+    
     //set the stars by putting them in an array and using a nice loop to set them
     NSArray* stars;
     stars = [[NSArray alloc] initWithObjects:star1, star2, star3, star4, star5, nil];
@@ -85,19 +43,6 @@
             ((UIImageView*)[stars objectAtIndex:i]).image = [UIImage imageNamed:@"Star_ON.png"];
         }
     }
-    
-    //setting the stars with good old if statements
-//    if ([restaurant averageCustomerReview]>=0.5)
-//        star1.image = [UIImage imageNamed:@"Star_ON.png"];
-//    if ([restaurant averageCustomerReview]>=1.5)
-//        star2.image = [UIImage imageNamed:@"Star_ON.png"];
-//    if ([restaurant averageCustomerReview]>=2.5)
-//        star3.image = [UIImage imageNamed:@"Star_ON.png"];
-//    if ([restaurant averageCustomerReview]>=3.5)
-//        star4.image = [UIImage imageNamed:@"Star_ON.png"];
-//    if ([restaurant averageCustomerReview]>=4.5)
-//        star5.image = [UIImage imageNamed:@"Star_ON.png"];
-    
     
     addressLabel.text = [restaurant address];
     navigationHeader.title = [restaurant name];
--- Restaurants/MasterViewController.m
+++ Restaurants/MasterViewController.m
@@ -9,6 +9,7 @@
 @implementation MasterViewController
 
 
+
 - (void)awakeFromNib
 {
     [super awakeFromNib];
@@ -26,6 +27,54 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    //set up the restaurants
+    Restaurant* piopio;
+    
+    piopio = [[Restaurant alloc] init];
+    
+    piopio.name = @"Pio Pio";
+    piopio.address = @"746 First Avenue\nNew York, NY 10128";
+    piopio.cuisineType = @"Peruvian";
+    piopio.yearOpened = 1995;
+    
+    Review* review1 = [[Review alloc] init];
+    review1.text = @"What fab-u-lass chicken! We could eat it all day if we didn't have to stop to drink sangria!";
+    review1.reviewer = @"The Addams";
+    review1.score = 5;
+    review1.numberOfHelpfulReviews = 19;
+    review1.numberOfUnhelpfulReviews = 8;
+    
+    Review* review2 = [[Review alloc] init];
+    review2.text = @"I DONE POSTED ON DA INTARWEBS!";
+    review2.reviewer = @"Anonymous";
+    review2.score = 1;
+    review2.numberOfHelpfulReviews = 0;
+    review2.numberOfUnhelpfulReviews = 45;
+    
+    Review* review3 = [[Review alloc] init];
+    review3.text = @"Some of the best chicken I've ever eaten. A helpful tip: get some green (Aji) sauce to go, they sell it by the pint!";
+    review3.reviewer = @"Jim Carr";
+    review3.score = 5;
+    review3.numberOfHelpfulReviews = 28;
+    review3.numberOfUnhelpfulReviews = 2;
+    
+    Review* review4 = [[Review alloc] init];
+    review4.text = @"While the food is amazing, they often simply don't pick up the phone when ordering out!";
+    review4.reviewer = @"Paul";
+    review4.score = 4;
+    review4.numberOfHelpfulReviews = 14;
+    review4.numberOfUnhelpfulReviews = 5;
+    
+    piopio.reviews = [[NSMutableArray alloc] init];
+    [piopio addReview:review1];
+    [piopio addReview:review2];
+    [piopio addReview:review3];
+    [piopio addReview:review4];
+    //toss the reviews into the array
+    //restaurant.reviews = [[NSArray alloc] initWithObjects:review1, review2, review3,review4, nil];
+    
+    
 }
 
 - (void)viewDidUnload
