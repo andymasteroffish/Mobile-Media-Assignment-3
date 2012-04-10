83f8cb54aaceb13be24e3456fc756dd76d1b8a76
andymasteroffish <andymasteroffish@gmail.com>
2012-04-09 20:06:05 -0400

    Assignment 5 - Testing The Cell (Gleaming The Cube)

---
+++ Restaurants/ReviewViewController.h
@@ -0,0 +1,13 @@
+//
+//  ReviewViewController.h
+//  Restaurants
+//
+//  Created by Andy Wallace on 4/9/12.
+//  Copyright (c) 2012 AndyMakes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReviewViewController : UITableViewController
+
+@end
